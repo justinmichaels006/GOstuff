0655090aef1873046ceb6e32dd0cfa5d680fd2f3
justinmichaels006 <justin@couchbase.com>
2016-04-12 19:03:51 -0700

    add the reader

--- src/github.com/user/reader/datareader.go
+++ src/github.com/user/reader/datareader.go
@@ -1,16 +1,59 @@
 package main
 
 import (
-	"gopkg.in/couchbase/gocb.v1"
 	"fmt"
 	"os"
-	"bufio"
+	"strconv"
+	"gopkg.in/couchbase/gocb.v1"
+	//"gopkg.in/matryer/try.v1"
+	//"log"
+	"time"
 )
 
 func checker(e error) {
 	if e != nil {
+		fmt.Println(e)
 		panic(e)
 	}
+}
+
+// Shut down.
+func done() {
+	fmt.Println("Good Bye")
+	os.Exit(101)
+}
+
+func getOne(val string, myBucket *gocb.Bucket, ch chan bool) { //(bool, error)
+
+	var err error
+	//var status bool
+	//var itemsGet []gocb.BulkOp
+	var theDoc map[string]interface{}
+
+	// Retrieve Document
+	_, err = myBucket.Get(val, &theDoc)
+	if err != nil {
+		fmt.Println("Doh! ", val)
+		//status = false
+		getOne(val, myBucket, ch)
+	}
+	if err == nil {
+		//itemsGet = append(itemsGet, &gocb.GetOp{Key: val, Value: &theDoc{}})
+		//Add time stamp for when this occured
+		//Update log file? or maybe the doc itself
+		now := time.Now()
+		nanos := now.UnixNano()
+		millis := nanos / 1000000
+		theDoc["getStamp"] = millis
+		myBucket.Upsert(val, theDoc, 0)
+		fmt.Println("Got It:", val)
+		//status = true
+		ch <- true
+		return
+	}
+
+	//return status, err
+
 }
 
 func main() {
@@ -18,28 +61,33 @@ func main() {
 	myC, _ := gocb.Connect("couchbase://10.111.94.102")
 	myB, _ := myC.OpenBucket("testload", "")
 
-	//Account Reader
-	f, err := os.OpenFile("/Users/justin/Documents/SVB/SVBaccounts.json", os.O_RDONLY, 0600)
-	checker(err)
+	var opsGroups = 5
+	var i = 0
+	ch := make(chan bool)
 
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
-		var myDoc interface{}
-		custID := scanner.Text()
-		custID = ("CUST::" + custID)
-		myB.Get(custID, &myDoc)
-		fmt.Println(custID)
+	for i < opsGroups {
+
+		str := strconv.Itoa(i)
+		//var rtnValue bool
+		var key string
+
+		key = ("ACCT::"+str)
+
+		go getOne(key, myB, ch)
+
+		//One way to do it
+/*		err := try.Do(func(attempt int) (bool, error) {
+			var err error
+			rtnValue, err = getOne(key, myB)
+			return attempt < 5, err // try 5 times
+		})*/
+		/*if err != nil {
+			log.Fatalln("error:", err)
+		}*/
+	i++
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
 
-
-
-	fmt.Println("Good Bye")
-	os.Exit(0)
+	<-ch
+	go done()
 }
-
-
 
