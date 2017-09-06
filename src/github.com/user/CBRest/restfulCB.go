6b333f3e57452bab277225c5a1efb8d54d97db05
justinmichaels006 <justin@couchbase.com>
2017-09-06 11:44:37 -0700

    Combine multi cluster scenario

--- src/github.com/user/CBRest/restfulCB.go
+++ src/github.com/user/CBRest/restfulCB.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 	"io"
 	"fmt"
-	"gopkg.in/couchbase/gocb.v1"
+	"github.com/couchbase/gocb"
 	"github.com/gorilla/mux"
 	"os"
 	"encoding/json"
+	"sync"
+	"strconv"
+	"time"
 )
 
 type User struct {
@@ -18,18 +21,13 @@ type User struct {
 
 var thedoc map[string]interface{}
 var err error
-var cluster, _ = gocb.Connect("couchbase://10.112.151.101")
-var bucket, _ = cluster.OpenBucket("beers", "")
-
-func main() {
-
-	rtr := mux.NewRouter()  //?.StrictSlash(true)
-	rtr.HandleFunc("/done", closeBucket)
-	rtr.HandleFunc("/POSTit/{id}/{doc}", postit) //?.Methods("POST")
-	rtr.HandleFunc("/GETit/{id}", getit) //?.Methods("GET")
-
-	http.ListenAndServe(":8888", rtr)
-}
+//Establish the environment to use
+var cluster, _ = gocb.Connect("couchbase://192.168.61.102/")
+var bucket, _ = cluster.OpenBucket("testload", "testload")
+var waitGroup sync.WaitGroup
+//Number is the number of bulk operations to perform
+var number int = 10
+var somejson map[string]interface{}
 
 func closeBucket(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Closed Bucket and Shutdown")
@@ -67,6 +65,91 @@ func postit(w http.ResponseWriter, r2 *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&thedoc)
+}
+
+func bulkloader(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("loop check")
+	// Retrieve Document
+	_, err = bucket.Get("dummy", &somejson)
+	if err != nil {
+		fmt.Println("Doh!")
+	}
+
+	waitGroup.Add(number)
+	// Channel our data so it can be picked up by an available goroutine
+	for i := 0; i < number; i++ {
+		go aworker(strconv.Itoa(i), somejson)
+	}
+
+	waitGroup.Wait()
+
+	fmt.Println("The loader application has completed!")
+}
+
+func aworker(id string, adoc map[string]interface{}) {
+	defer waitGroup.Done()
+	fmt.Println("debug check")
+	// Create the document
+	adoc["upStamp"] = nil
+	bucket.Upsert(id, adoc, 0)
+
+	// Append to an array or create if it doesn't exist, using a subdocument operation
+	now := time.Now()
+	nanos := now.UnixNano()
+	millis := nanos / 1000000
+	bucket.MutateIn(id, 0, 0).Upsert("upStamp", millis, true).Execute()
+
+}
+
+func bulkreader(w http.ResponseWriter, r *http.Request) {
+
+	waitGroup.Add(number)
+	// Channel our data so it can be picked up by an available goroutine
+	for i := 0; i < number; i++ {
+		go bworker(i, somejson)
+	}
+
+	waitGroup.Wait()
+
+	fmt.Println("The reader application has completed!")
+}
+
+func bworker(thenum int, bdoc map[string]interface{}) {
+
+	defer waitGroup.Done()
+	fmt.Println("debug check")
+
+	id := strconv.Itoa(thenum)
+
+	for {
+		// Retrieve Document
+		_, err = bucket.Get(id, &bdoc)
+		if err != nil {
+			fmt.Println("Doh! ", id)
+			//time.Sleep(100000)
+			//go bworker(thenum, bdoc)
+		} else {
+			now := time.Now()
+			nanos := now.UnixNano()
+			millis := nanos / 1000000
+			bucket.MutateIn(id, 0, 0).Upsert("getStamp", millis, true).Execute()
+			fmt.Println("Got It:", id)
+			break
+		}
+	}
+}
+
+func main() {
+
+	rtr := mux.NewRouter()  //?.StrictSlash(true)
+	rtr.HandleFunc("/done", closeBucket)
+	rtr.HandleFunc("/POSTit/{id}/{doc}", postit) //?.Methods("POST")
+	rtr.HandleFunc("/GETit/{id}", getit) //?.Methods("GET")
+	rtr.HandleFunc("/POSTbulk/", bulkloader)
+	rtr.HandleFunc("/GETbulk/", bulkreader)
+
+	http.ListenAndServe(":8888", rtr)
 }
 
 
